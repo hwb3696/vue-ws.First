a22aeacb69e2812c9a8660a3985ca6ef56d33416
huwenbin <wyhuwenbin@163.com>
2021-03-22 17:20:00 +0800

    项目搭建

---
+++ src/page/home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <head-top></head-top>
+    <section class="data_section"></section>
+    <tendency :sevenDate="sevenDate" :sevenDay="sevenDay"></tendency>
+  </div>
+</template>
+
+<script>
+import headTop from "../components/headTop";
+import tendency from "../components/tendency";
+import dtime from "time-formater";
+// import {userCount, orderCount, getUserCount, getOrderCount, adminDayCount, adminCount} from '@/api/getData'
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    headTop,
+    tendency,
+  },
+  mounted() {},
+  computed: {},
+  methods: {},
+};
+</script>
+
+<style lang="less">
+@import "../style/mixin";
+.data_section {
+}
+.wan {
+  .sc(16px, #333);
+}
+</style>
